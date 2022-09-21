10f18d91c790d402ddfbefbaf02d42fd1d3801cd
Kovács Dorina <kovacsdorina2@gmail.com>
2022-09-21 10:56:21 +0200

    Törlés Hozzáadás gomb

--- src/app.js
+++ src/app.js
@@ -1,26 +1,84 @@
 // const { result } = require("lodash");
 
 const dowButton = document.querySelector("#dowButton")
+const addButton = document.querySelector("#addButton")
+const empTable = document.querySelector("#empTable")
+const empName = document.querySelector("#empName");
 
+
+const host = "http://localhost:3000";
 
 dowButton.addEventListener('click' ,() => {
-    console.log("Letöltés");
+    // console.log("Letöltés");
+    let endpoint = 'employees';
+    let url = host + '/' + endpoint;
+    fetch(url)
+    .then( response => response.json())
+    .then( result => {
+        console.log(result[0].name );
+        renderTable(result);
+    })
+    .catch(error => {
+        console.log("Hiba! A lekérdezés sikertelen!");
+        console.log(error);
+    })
+});
+
+function renderTable(employees){
+    // console.log(employees[1].name);
+
+    employees.forEach(employee => {
+        // console.log(employee.name);
+        let tr = document.createElement('tr');
+        let tdId = document.createElement('td');
+        let tdName = document.createElement('td');
+        let tdDel = document.createElement('td');
+        let delBtn = document.createElement('button');
+        delBtn.textContent = 'Törlés';
+        delBtn.addEventListener('click',() => {
+            deleteEmployees = employee.id;
+        });
+
+        tr.appendChild(tdId);
+        tr.appendChild(tdName);
+        tr.appendChild(tdDel)
+        tdDel.appendChild(delBtn);
+        empTable.appendChild(tr);
+
+
+        tdId.textContent = employee.id;
+        tdName.textContent = employee.name;
+    });
+}
+  
+addButton.addEventListener('click' ,() => {
+    let endpoint = 'employees';
+    let url = host + '/' + endpoint;
+    let employee = {
+
+        name: empName.value
+    };
+    fetch(url, {
+        method: 'post',
+        body: JSON.stringify(employee),
+        headers: {
+            
+            'Content-Type':'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+    })
 })
-
-var url = "http://localhost:3000/employees";
-
-fetch(url)
-.then( (response) =>
-     response.json()
-.then( result => console.log(result[0].name ))
-.catch(error => {
-    console.log("Hiba! A lekérdezés sikertelen!");
-    console.log(error);
-})
-);
-
-
-// .then( result => result.json())
-// .then( data => {
-//     console.log(data);
-// });
+  
+function deleteEmployees(id){
+    console.log(id);
+    let endpoint = 'employees';
+    let url = host + '/' + endpoint + '/' + id;
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+    });
+}
